5489a5058728c0829898f82a066f5ce8b93b4ccc
Bill Donner <billdonner@gmail.com>
2020-02-02 19:00:43 -0500

    firt sommit

--- Sources/URLSurfer/URLSurfer.swift
+++
@@ -1,3 +0,0 @@
-struct URLSurfer {
-    var text = "Hello, World!"
-}
--- Tests/URLSurferTests/URLSurferTests.swift
+++ Tests/URLSurferTests/URLSurferTests.swift
@@ -6,10 +6,21 @@ final class URLSurferTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(URLSurfer().text, "Hello, World!")
+        XCTAssertEqual(URLSurfer().text, "URLSurfer")
+    }
+    func testIsa(){
+        
+        let vc =             URLSurfPicker([URL(string:"https://apple.com")!,
+                                 URL(string:"https://google.com")!])
+        
+        // now what?
+        
+    XCTAssertTrue(vc is UINavigationController)
+        
     }
 
     static var allTests = [
         ("testExample", testExample),
+          ("testIsa", testIsa),
     ]
 }
